0c91e29cf51f85635d540a1cfa40c93a1415dc05
Wiktor <wiktor.zycho99@gmail.com>
2021-09-12 17:38:40 +0200

    New version with learning rates

--- main.py
+++ main.py
@@ -1,6 +1,60 @@
 import tensorflow.keras as k
 from random import shuffle
 import sys
+import json
+import numpy as np
+
+class CustomCallback(k.callbacks.Callback):
+    epochs = {"Learning_rate":[], "Loss":[], "Weights":[]}
+
+    def on_train_begin(self, logs=None):
+        keys = list(logs.keys())
+        print(f"Starting training; got log keys: {keys}")
+        print(50 * '*')
+
+    def on_train_end(self, logs=None):
+        keys = list(logs.keys())
+        print(f"Stop training; got log keys: {keys}")
+        print(50 * '*')
+
+    def on_epoch_begin(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print(f"\nStart epoch {epoch} of training; got log keys: {keys}")
+        lr = float(k.backend.get_value(self.model.optimizer.learning_rate))
+        print(f"Learning rate: {lr}")
+        CustomCallback.epochs['Learning_rate'].append(lr)
+        print(50 * '-')
+
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print(f"\nEnd epoch {epoch} of training; got log keys: {keys}")
+        layers = []
+        deltas = []
+        for i, l in enumerate(self.model.layers):
+            print(f"warstwa {i}")
+            print(l.weights)
+            layers.append([l.weights[0].numpy(), l.weights[1].numpy()])
+            # print("What")
+            # print(layers[0][0])
+            # if epoch == 0:
+            #     deltas.append([l.weights[0].numpy(), l.weights[1].numpy()])
+            # else:
+            #     deltas.append([np.subtract(l.weights[0].numpy(),layers[epoch-1][0]),
+            #     np.subtract(l.weights[1].numpy(),layers[epoch-1][1])])
+
+        CustomCallback.epochs['Weights'].append(layers)
+
+        
+        # CustomCallback.epochs['Weights_delta'].append(deltas)
+            
+
+        print( "The average loss for epoch {} is {:7.2f}."
+        .format(epoch, logs["loss"]))
+        CustomCallback.epochs['Loss'].append(logs["loss"])
+
+    print(50 * "-")
+
+
 
 def iris_to_label(iris):
     if iris == 'Iris-setosa':
@@ -33,59 +87,54 @@ def read_data(filename):
             [ iris_to_label(line[-1]) for line in data ]
         )
 
-def get_weights(layers, epoch, logs):
 
-    data_from_current_epoch = []
-
-    for i, layer in enumerate(layers):
-
-        neurons = (layer.weights)[0].numpy()
-        bias = (layer.weights)[1].numpy()
-
-        layer_data = [neurons, bias]
-
-        data_from_current_epoch.append(layer_data)
-
-    return data_from_current_epoch
-
-##########################################################################################
-
-neurons = sys.argv[1:] # pobranie liczby neuronów w warstwach (np. 8 8 w wywolaniu
+neurons = sys.argv[1:3] # pobranie liczby neuronów w warstwach (np. 8 8 w wywolaniu
                        # tworzy dwie ukryte warstwy, kazda po 8 neuronów)
 
 
 # czytanie danych o irysach
 x, y = read_data('iris.data')
 
-x_train = x[:140]
-x_test = x[140:]
+x_train = x[:120]
+x_test = x[120:]
 
-y_train = y[:140]
-y_test = y[140:]
+y_train = y[:120]
+y_test = y[120:]
 
-input_size = len(x[0])
-output_size = len(y[0])
 
 # tworzenie sieci
 model = k.Sequential()
 
 # pierwsza warstwa ukryta (input_dim informuje o rozmiarze danych na wejściu)
-model.add(k.layers.Dense(units=neurons[0], input_dim=input_size, activation='relu'))
+model.add(k.layers.Dense(units=neurons[0], input_dim=4, activation='relu'))
 
 # kolejne warstwy ukryte
 for neuron in neurons[1:]:
+    print(neuron)
     model.add(k.layers.Dense(units=neuron, activation='relu'))
 
 # ostatnia warstwa, units=3 bo na wyjściu wybieramy z trzech różnych typów irysów
-model.add(k.layers.Dense(units=output_size, activation='sigmoid'))
+model.add(k.layers.Dense(units=3, activation='relu'))
 
 # kompilacja sieci
 model.compile(optimizer='sgd', loss='mse')
 
 # tutaj ustawiamy funkcję, którajest wywoływana przy każdej epoce podczas treningu
 # na razie tylko są wypisywane poszczególne wagi i biasy
-weights = []
-epoch_end = k.callbacks.LambdaCallback(on_epoch_end=lambda e, l: weights.append(get_weights(model.layers, e, l)))
+callback = CustomCallback()
+# epoch_end = k.callbacks.LambdaCallback(on_epoch_end=lambda e, l: print_weights(model.layers, e, l))
 
 # trening sieci
-model.fit(x_train, y_train, epochs=100, callbacks=epoch_end)
+e = int(sys.argv[3])
+model.fit(x_train, y_train, epochs=e, callbacks=callback)
+
+# sprawdzamy wyniki, w sumie nie istotne dla naszego projektu
+y_pred = model.predict(x_test)
+
+print("\n***Resutls***")
+print(y_pred)
+print(y_test)
+
+print(CustomCallback.epochs)
+# with open("data.json", "w") as f:
+#     json.dump(CustomCallback.epochs, f, indent=4)
